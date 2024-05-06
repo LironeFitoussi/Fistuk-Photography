c87c6fa9ba08e596d39d7e1d80415cb6871484f1
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-05-06 19:19:25 +0300

    Dispalying collections

--- Client/src/routes/Photography/Photography.tsx
+++ Client/src/routes/Photography/Photography.tsx
@@ -1,14 +1,42 @@
 import { Outlet } from 'react-router-dom';
 import styles from './Photography.module.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Collection {
+    date: string;
+    description: string;
+    images: string[]; // Define a more specific type instead of 'any' if the image structure is known
+    location: string;
+    name: string;
+    __v: number;
+    _id: string;
+}
 
 const Photography = () => {
     const [activeLink, setActiveLink] = useState('collections');
+    const [collections, setCollections] = useState([]);
+    // const [albums, setAlbums] = useState([]); // TODO: Fetch albums from server
 
     const handleLinkClick = (link: string) => {
         setActiveLink(link);
     };
 
+    // Fetch coleections names from Server 
+    useEffect(() => {
+        // Fetch collections in async function
+        const collections = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/api/v1/collections');
+                console.log(response.data.data.collections);
+                setCollections(response.data.data.collections);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        collections();
+    }
+    , []);
     return (
         <div>
             <nav >
@@ -25,7 +53,14 @@ const Photography = () => {
             {activeLink === 'collections' && (
                 <div>
                     <p>dfsgsdgfsdg</p>
-                    {/* Render collections */}
+                    {/* Renders a list of Collection names with link to /collections/:collectionId */}
+                    <ul>
+                        {collections.map((collection: Collection) => (
+                            <li key={collection._id}>
+                                <a href={`/photography/collections/${collection._id}`}>{collection.name}</a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             )}
 
