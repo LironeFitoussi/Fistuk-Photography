ca1b02ef5125219492471ba50fa3994f38694019
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-05-07 02:06:11 +0300

    Uploading progression bar

--- Server/src/controllers/imagesController.ts
+++ Server/src/controllers/imagesController.ts
@@ -1,6 +1,8 @@
+/// <reference types="progress" />
 import { Request, Response } from 'express';
 import Image from '../models/Image.model';
 import Collection from '../models/Collections.model';
+import ProgressBar from 'progress';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -14,21 +16,7 @@ import {
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 import s3 from '../s3';
 
-
-// // Safely retrieve environment variables
 const bucketName = process.env.AWS_BUCKET_NAME
-// const region = process.env.AWS_REGION 
-// const accessKeyId = process.env.AWS_ACCESS_KEY || ""
-// const secretAccessKey = process.env.AWS_SECRET_KEY || ""
-
-// const s3 = new S3Client({
-//   region,
-//   credentials: {
-//     accessKeyId,
-//     secretAccessKey
-//   }
-// });
-
 interface CustomFile extends File {
     buffer: Buffer;
     originalname: string;
@@ -43,55 +31,66 @@ interface CustomRequest extends Request {
 const randomIamgeName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
 
 export const createImage = async (req: CustomRequest, res: Response) => {
-    const { collectionName } = req.body;
-    console.log('req.body', req.body);
-    console.log('collectionName', collectionName);
-    
-    const collection = await Collection.findOne({ name: collectionName });
+  const { collectionName } = req.body;
+  console.log('req.body', req.body);
+  console.log('collectionName', collectionName);
 
-    if (!collection) {
-        console.log('Collection not found');
-        return res.status(404).json({ message: ': Collection not found' });
+  const collection = await Collection.findOne({ name: collectionName });
+
+  if (!collection) {
+    console.log('Collection not found');
+    return res.status(404).json({ message: ': Collection not found' });
+  }
+
+  const collectionId = collection._id;
+  const totalFiles = req.files?.length || 0;
+  const progressBar = new ProgressBar(`Uploading files [:bar] :current/:total :percent :etas`, {
+    total: totalFiles,
+    width: 30,
+    complete: '=',
+    incomplete: ' ',
+    renderThrottle: 100, // Update the progress bar every 100ms
+  });
+
+  const uploadPromises = req.files?.map(async (file, index) => {
+    const fileName = `${randomIamgeName()}.jpeg`;
+    const params = {
+      Bucket: bucketName,
+      Key: fileName,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
+
+    const command = new PutObjectCommand(params);
+
+    try {
+      await s3.send(command);
+      const image = new Image({
+        filename: fileName,
+        name: file.originalname,
+        collectionId,
+      });
+      await image.save();
+      progressBar.tick();
+    } catch (error) {
+      console.error('Failed to upload the image', error);
     }
+  });
 
-    req.files?.forEach(async (file) => {
-        const fileName = `${randomIamgeName()}.jpeg`
-        const params = {
-            Bucket: bucketName,
-            Key: fileName,
-            Body: file.buffer,
-            ContentType: file.mimetype,
-        };
-
-        
-        const command = new PutObjectCommand(params);
-        try {
-            const data = await s3.send(command);
-            console.log('Successfully uploaded the image');
-            // Find the collections id fro the request body description using mongoose
-            // console.log(data);
-            
-            const collectionId = collection._id;
-            // console.log('collectionId', collectionId);
-            console.log(fileName);
-            
-            
-            const image = new Image({
-                filename: fileName,
-                name: file.originalname,
-                collectionId: collectionId,
-            });
-
-            console.log('image', image);
-            
-            await image.save();
-            res.status(201).json({ message: 'All images uploaded successfully', results: image });
-        } catch (error) {
-            console.error('Failed to upload the image', error);
-        }
-    });   
+  if (uploadPromises && uploadPromises.length > 0) {
+    try {
+      await Promise.all(uploadPromises);
+      console.log('\nSuccessfully uploaded all images');
+      progressBar.terminate(); // Stop the progress bar
+      res.status(201).json({ message: 'All images uploaded successfully' });
+    } catch (error) {
+      console.error('Error uploading images:', error);
+      res.status(500).json({ message: 'An error occurred while uploading images' });
+    }
+  } else {
+    res.status(400).json({ message: 'No files provided' });
+  }
 };
-
 
 export const getAllImages = async (req: Request, res: Response) => {
     try {
