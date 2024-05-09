e127e96096b6aa232efe57fadbeee766988450f5
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-05-10 02:56:34 +0300

    Revert "test"
    This reverts commit 2126c6d337bd00c6fbf972cf06bdad13fa13e69b.

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,56 +1,23 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    reactRefresh(),
+    react(),
     legacy({
       targets: ['defaults', 'not IE 11']
     })
   ],
-  optimizeDeps: {
-    include: [
-      '@emotion/react',
-      '@emotion/styled',
-      '@mui/icons-material',
-      '@mui/material',
-      '@mui/x-data-grid',
-      'aws-sdk',
-      'axios',
-      'dateformat',
-      'globalthis',
-      'react',
-      'react-aws-s3-typescript',
-      'react-dom',
-      'react-grid-gallery',
-      'react-icons',
-      'react-router-dom'
-    ],
-    exclude: ['@emotion/react', '@emotion/styled'] // Exclude these from optimization if needed
-  },
   build: {
-    chunkSizeWarningLimit: 1000, // Adjust chunk size warning limit as needed
-    rollupOptions: {
-      manualChunks: {
-        vendor: [
-          '@emotion/react',
-          '@emotion/styled',
-          '@mui/icons-material',
-          '@mui/material',
-          '@mui/x-data-grid',
-          'aws-sdk',
-          'axios',
-          'dateformat',
-          'globalthis',
-          'react',
-          'react-aws-s3-typescript',
-          'react-dom',
-          'react-grid-gallery',
-          'react-icons',
-          'react-router-dom'
-        ]
-      }
-    }
+    // Adjust the asset chunk size limit (in bytes)
+    assetsInlineLimit: 5000000, // Set the limit to 1 MB (1000 kB)
+  },
+  optimizeDeps: {
+    include: ['lodash', 'axios'],
+  },
+  define: {
+    global: 'globalThis' // This sets up 'global' as an alias to 'globalThis'
   }
 });
