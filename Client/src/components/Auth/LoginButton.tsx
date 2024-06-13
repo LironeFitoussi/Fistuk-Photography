b5e7d01fb2c6295d18319459b11f649f45cb8161
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-06-13 22:45:48 +0300

    chore: Update LoginButton and LogoutButton components to use Material-UI Button component

--- Client/src/components/Auth/LoginButton.tsx
+++ Client/src/components/Auth/LoginButton.tsx
@@ -1,9 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import Button from '@mui/material/Button';
 
 const LoginButton: React.FC = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <Button variant="contained" color="success" onClick={() => loginWithRedirect()}>Log In</Button>;
 };
 
 export default LoginButton;
