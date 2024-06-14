a4a620c517dce59fe4ef1b483c6e0d2fe4184808
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-06-14 14:34:05 +0300

    CSS Tuning

--- Client/src/components/ProjectCard/ProjectCard.tsx
+++ Client/src/components/ProjectCard/ProjectCard.tsx
@@ -3,30 +3,41 @@ import styles from "./ProjectCard.module.css";
 import { FaGithub } from "react-icons/fa";
 import { LuScreenShare } from "react-icons/lu";
 
-
 interface ProjectCardProps {
-    project: Project;
+  project: Project;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
-    return (
-        <div className={styles.container}>
-            <img src={project.previewImage} alt={project.title} />
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-            <h3>Teachnologies used</h3>
-            <div className={styles.stack}>
-                {project.technologies.map((tech, index) => (
-                    <i className={`devicon-${tech.toLowerCase()}-original devicon-${tech.toLowerCase()}-plain`} key={index} title={`${tech}`}></i>
-                ))}
-            </div>
-            <div className={styles.devider}></div>
-            <div className={styles.links}>
-                <a href={project.demoLink} target="_blank">Live Site <LuScreenShare/></a>
-                <a href={project.githubLink} target="_blank">Github <FaGithub/></a>
-            </div>
+  return (
+    <div className={`${styles.container} ${project.projectType === 'main-project' && styles.mainProject}`}>
+      <div>
+        <img src={project.previewImage} alt={project.title} />
+        <h2>{project.title}</h2>
+        <p>{project.description}</p>
+      </div>
+      <div>
+        <h3>Teachnologies used</h3>
+        <div className={styles.stack}>
+          {project.technologies.map((tech, index) => (
+            <i
+              className={`devicon-${tech.toLowerCase()}-original devicon-${tech.toLowerCase()}-plain`}
+              key={index}
+              title={`${tech}`}
+            ></i>
+          ))}
         </div>
-    );
-}
+        <div className={styles.devider}></div>
+        <div className={styles.links}>
+          <a href={project.demoLink} target="_blank">
+            Live Site <LuScreenShare />
+          </a>
+          <a href={project.githubLink} target="_blank">
+            Github <FaGithub />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectCard;
